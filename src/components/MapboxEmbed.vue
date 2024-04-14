22e35f79f2e64166ce3523eb5b45d42d6a3e8ab3
Max Stein <maxwell.stein@gmail.com>
2024-04-14 15:34:57 -0700

    centers on draggableMarkerCoordinates on toggle

--- src/components/MapboxEmbed.vue
+++ src/components/MapboxEmbed.vue
@@ -147,6 +147,7 @@ watch(
   () => props.showDraggableMarker,
   () => {
     props.showDraggableMarker ? createDraggableMarker(props.draggableMarkerCoordinates || map.value.getCenter()) : removeDraggableMarker()
+    props.showDraggableMarker && props.draggableMarkerCoordinates && map.value.setCenter(props.draggableMarkerCoordinates)
   },
 )
 
